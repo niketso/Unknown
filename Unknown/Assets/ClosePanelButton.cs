c366214b005f2197aa7a5025c4518d742e286bf9
Unknown <li_gueva@hotmail.com>
2018-11-23 12:43:13 -0300

    Intro Panel

---
+++ Unknown/Assets/ClosePanelButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClosePanelButton : MonoBehaviour {
+
+    GameObject panel;
+
+    private void Awake()
+    {
+        Time.timeScale = 0;
+        var btn = GetComponent<Button>();
+        btn.onClick.AddListener(ClosePanel);
+        panel = transform.parent.gameObject;
+    }
+
+    void ClosePanel()
+    {        
+        panel.SetActive(false);
+        Time.timeScale = 1;        
+    }
+}
